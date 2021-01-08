4ca9c297972dabc5a6acc52077c1ad62d9c56af6
Mateusz Glowania <mateusz.glowania@gmail.com>
2021-01-08 16:37:35 +0100

    prototype

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector('nav')
+const handleClick = () =>{
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('nav--active')
+};
+hamburger.addEventListener('click',handleClick);
