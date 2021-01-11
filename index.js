8f622136ae5c23c2742869c1556a06b0516293f5
Mateusz Glowania <mateusz.glowania@gmail.com>
2021-01-11 19:26:01 +0100

    Repair menu

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector('nav')
+const nav = document.querySelector('.nav')
 const handleClick = () =>{
     hamburger.classList.toggle('hamburger--active');
     nav.classList.toggle('nav--active')
+    nav.style.position = "fixed";
 };
 hamburger.addEventListener('click',handleClick);
 
-const navLinks = document.querySelectorAll('nav__links');
+const navLinks = document.querySelectorAll('.nav__links');
 navLinks.forEach((element)=>{
-    addEventListener('click',()=>{
+    element.addEventListener('click',()=>{
+        console.log(element)
     nav.classList.remove('nav--active');
     hamburger.classList.remove('hamburger--active')
 });
